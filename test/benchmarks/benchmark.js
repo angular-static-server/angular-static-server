bf3cce1758ac846d303adcd0264d04bdb570df4a
Lukas Spirig <lukas.spirig@gmail.com>
2023-08-17 16:26:32 +0200

    chore: updated benchmark data

--- test/benchmarks/benchmark.js
+++ test/benchmarks/benchmark.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.3/index.js';
 
 export const options = {
   vus: 100,
@@ -15,5 +16,6 @@ export default function () {
 export function handleSummary(data) {
   return {
     [`k6-${__ENV.TYPE || ''}-report.html`]: htmlReport(data),
+    [`k6-${__ENV.TYPE || ''}-report.txt`]: textSummary(data, { indent: '→', enableColors: false }),
   };
 }
